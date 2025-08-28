462f4006740292d94f7ce98c6b0d18be1eb273d1
Daniel <danielrodrigues@ioasys.com.br>
2025-08-28 20:00:13 -0300

    Initial Commit

---
+++ dandori/dandoriApp.swift
@@ -0,0 +1,20 @@
+//
+//  dandoriApp.swift
+//  dandori
+//
+//  Created by Daniel Rodrigues on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct dandoriApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
